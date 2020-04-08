24b7e7b4af9bf576fa826c56db99938bb036ea22
Leonardo Sanna <leonardo.sanna@unimore.it>
2020-04-08 13:24:20 +0200

    correzione nel subsampling, "&" per usare due condizioni insieme

--- Test week 1.R
+++ Test week 1.R
@@ -7,9 +7,9 @@ tail(test, 2) #print the last two lines
 test$Ozone[47] #extract the value in the column Ozone row 47
 sum(is.na(test$Ozone)) #count all missing values
 mean(test$Ozone, na.rm = TRUE) #mean without Na
-sampling <- subset(test, Ozone > 31 | Temp > 90, select = c("Ozone","Temp", "Solar.R")) #seleziona un sample di valori: maggiore di 31 in Ozone, Maggiore di 90 in Temp
+sampling <- subset(test, Ozone > 31 & Temp > 90, select = c("Ozone","Temp", "Solar.R")) #seleziona un sample di valori: maggiore di 31 in Ozone, Maggiore di 90 in Temp
 print(sampling)
-second_sample <- subset(sampling, Temp > 90)
+second_sample <- subset(sampling, Temp > 90)my
 print(second_sample)
 mean(second_sample$Solar.R, na.rm= TRUE) #fa la media del sample nella colonna solar R_
 
